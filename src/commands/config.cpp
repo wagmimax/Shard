cad7bc55150152272b883e3a22a991d6606a605a
Maxim Kazak <wagmimax@gmail.com>
2026-01-19 14:56:18 -0500

    config delete function

--- src/commands/config.cpp
+++ src/commands/config.cpp
@@ -4,8 +4,7 @@
 
 //create and add a new command to config.json. new commands are automatically set to the current directory you are in
 static void setShardCommand(shard::args cmdArgs) {
-    //available commands:
-    //shard --config
+    //mock command:
     //shard --config set <name> as <command>
 
     //cmdArgs[0] shard         
@@ -43,6 +42,43 @@ static void setShardCommand(shard::args cmdArgs) {
     inFile.close();
 }
 
+//remove a shard command from the config
+static void deleteShardCommand(shard::args cmdArgs) {
+    //mock command:
+    //shard --config delete <name>
+
+    //cmdArgs[0] shard         
+    //cmdArgs[1] --config                           
+    //cmdArgs[2] delete                            
+    //cmdArgs[3] {user defined name for the command}
+
+    if(cmdArgs.size() != 4) {
+        std::cout << "shard --config delete <name>" << std::endl;
+        return;
+    }
+
+    //load existing config json
+    std::ifstream inFile(getExecutablePath() / "config.json");
+    nlohmann::json configJson;
+    inFile >> configJson;
+
+    //delete the command
+    std::string command = cmdArgs[3];
+    configJson["directories"][std::filesystem::current_path().string()].erase(command);
+
+    //truncate and write to config.json
+    std::ofstream outFile(getExecutablePath() / "config.json", std::ios::trunc);
+    outFile << configJson.dump(4);
+
+    outFile.close();
+    inFile.close();
+}
+
+//delete directories and their commands if they no longer exist
+static void purgeShardCommands(shard::args cmdArgs) {
+
+}
+
 //upon first time calling shard --config
 static void createConfigJson(std::filesystem::path path) {
     std::ofstream configJson(path / "config.json");
@@ -54,15 +90,20 @@ static void createConfigJson(std::filesystem::path path) {
     configJson << json;
 }
 
-static constexpr std::array configCommands = {shard::Command{"set", &setShardCommand}};
+static constexpr std::array configCommands = {shard::Command{"set", &setShardCommand},
+                                              shard::Command{"delete", &deleteShardCommand},
+                                              shard::Command{"purge", &purgeShardCommands}};
 
 void shard::config(args cmdArgs) {
-
 
     //check if config file exists. if not, create one
     if (!std::filesystem::exists(getExecutablePath() / "config.json")) {
         createConfigJson(getExecutablePath());
     }
 
-    setShardCommand(cmdArgs);
+    for(auto& command : configCommands) {
+        if(command.name == cmdArgs[2]) {
+            command.function(cmdArgs);
+        }
+    }
 }
